732bc8687ba39ce57b28c7e8ea7179f3ae7d0ca5
Venugopal Naik <venugopal.naik@oracle.com>
2021-01-06 10:21:09 +1100

    OCI Observability & Management Quickstart

---
+++ README.md
@@ -0,0 +1,8 @@
+# Oracle Cloud Infrastructure (OCI) Observability and Management Quick Start
+
+## Introduction
+
+This repository would include automation for various observability and management services on OCI.
+
+* OCI Logging Analytics
+* OCI Logging
---
+++ oci-logging-analytics/variables.tf
@@ -0,0 +1,13 @@
+# Copyright (c) 2021, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+
+# tenancy details
+variable "tenancy_id" {}
+variable "compartment_id" {}
+variable "user_id" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
+
